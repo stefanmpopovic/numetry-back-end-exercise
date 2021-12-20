32473524d295112555ea58a0e1f7f0d0d5b1678b
Carlos Fructuoso <analista.carlosh@gmail.com>
2021-12-20 22:48:28 +0000

    Update entity and migration

--- src/entity/Assignment.ts
+++ src/entity/Assignment.ts
@@ -14,17 +14,8 @@ export class Assignment {
   @Column({ name: 'end_period' })
   endPeriod!: Date;
 
-  @Column({ name: 'start_time' })
-  startTime!: Date;
-
-  @Column({ name: 'end_time' })
-  endTime!: Date;
-
-  @Column({ name: 'lock' })
-  lock!: Boolean;
-
   @Column({ default: true })
-  isActive!: boolean;
+  active!: boolean;
 
   @Column({ name: 'created_at'})
   createdAt!: Date;
---
+++ src/migration/1640036183948-Assigment.ts
@@ -0,0 +1,23 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class Assigment1640036183948 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    `
+    CREATE TABLE assignment (
+      id serial NOT NULL,
+      name VARCHAR ( 150 ) NOT NULL,
+      start_period DATE NOT NULL,
+      end_period DATE,
+      active BOOLEAN NOT NULL DEFAULT ( TRUE ),
+      created_at DATE NOT NULL DEFAULT ( CURRENT_TIMESTAMP ),
+      PRIMARY KEY ( id ) 
+    )
+    `
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(` DELETE FROM assignment `);
+    await queryRunner.query(` DROP TABLE assignment `);
+  }
+}
